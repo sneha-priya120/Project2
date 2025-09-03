43f4376b2fecd040d2075f9c3dc50a7ac775a1a2
Sneha Priya <Snehabambore@gmail.com>
2025-09-03 21:37:20 +0530

    Added sub function in math.c

--- math.c
+++ math.c
@@ -3,10 +3,15 @@ int add(int a,int b)
 {
 return a+b;
 }
+int sub(int c,int d)
+{
+return c-d;
+}
 int main()
 {
-int a=5,b=6;
+int a=5,b=6,c=6,d=5;
 printf("Sum is %d\n",add(a,b));
+printf("Sub is %d\n",sub(c,d));
 return 0;
 }
 
